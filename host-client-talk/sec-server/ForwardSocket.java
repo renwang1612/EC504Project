6c3282a67c59c7dd9614e674c85714c090ef18f6
bob-icsg <bobzhou@bu.edu>
2015-10-16 16:04:53 -0400

    AUTOCOMMIT bobzhou@bobzhou-ThinkCentre-M73:Fri Oct 16 10:27:52 EDT 2015

--- host-client-talk/sec-server/ForwardSocket.java
+++ host-client-talk/sec-server/ForwardSocket.java
@@ -17,6 +17,7 @@ class ForwardSocket implements Runnable {
             while ( true ) {
                 Scanner in      = new Scanner(inputSocket.getInputStream());
                 String input    = in.nextLine();
+                System.out.println("Client Said: " + input);//PRINT IT OUT TO THE SCREEN
                 System.out.println ( "forward msg:" + input ) ;
                 //IF THERE IS INPUT THEN MAKE A NEW VARIABLE input AND READ WHAT THEY TYPED
                 PrintWriter output = new PrintWriter(outputSocket.getOutputStream());

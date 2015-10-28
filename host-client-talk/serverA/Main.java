0c77670276239faa2ef32717d057c9829b9c92be
bob-icsg <bobzhou@bu.edu>
2015-10-28 12:50:59 -0400

    code cleaned

--- host-client-talk/serverA/Main.java
+++ host-client-talk/serverA/Main.java
@@ -21,43 +21,29 @@ public class Main {
             System.out.println("Waiting for clients...");
             //AT THE START PRINT THIS
 
-            Socket s        = server.accept();
+          	// Once the server is successfully connected with client
+		    Socket s        = server.accept();
             System.out.println("Client connected from " + s.getLocalAddress().getHostName());   
 
-            readFromCmd cmdline             = new readFromCmd ( s ) ;
-            msgSentFromClient readClient    = new msgSentFromClient ( s ) ;
-            Thread thread1                  = new Thread ( cmdline ) ;
-            Thread thread2                  = new Thread ( readClient ) ;
+            Send send             			= new Send ( s ) ;
+            Receive receive    				= new Receive ( s ) ;
+            Thread thread1                  = new Thread ( send ) ;
+            Thread thread2                  = new Thread ( receive ) ;
             thread1.start () ;
             thread2.start () ;
-         
-            // while (true) {//WHILE THE PROGRAM IS RUNNING 
-
-            //     Socket s        = server.accept();
-            //     //ACCEPT SOCKETS(CLIENTS) TRYING TO CONNECT
-            //     System.out.println("Client connected from " + s.getLocalAddress().getHostName());   
-            //     //  TELL THEM THAT THE CLIENT CONNECTED
-
-            //     Client chat     = new Client(s);
-            //     //CREATE A NEW CLIENT OBJECT
-            //     Thread t        = new Thread(chat);
-            //     //MAKE A NEW THREAD
-            //     t.start();
-            //     //START THE THREAD
-            // }
             
             Socket ServerClient             = new Socket ( HOST , ServerClientPORT ) ;
             System.out.println("Server connected");   
 
-            msgSentFromClient receive       = new msgSentFromClient ( ServerClient ) ;
-            Thread thread3                  = new Thread ( receive ) ;
+            Receive receiveFromServerB      = new Receive ( ServerClient ) ;
+            Thread thread3                  = new Thread ( receiveFromServerB ) ;
             thread3.start () ;
 
         }
 
         catch (Exception e) {
             System.out.println("An error occured.");
-            //IF AN ERROR OCCURED THEN PRINT IT
+            //If an error occured then print it
             e.printStackTrace();
         }
     }
--- host-client-talk/serverB/Send.java
+++ host-client-talk/serverB/Send.java
@@ -2,23 +2,27 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
-class readFromCmd implements Runnable {
+class Send implements Runnable {
 
     private Socket socket ;
 
-    public readFromCmd ( Socket s ) {
+    public Send ( Socket s ) {
         socket              = s ;
     }
 
     public void run () {
         try { 
             while ( true ) {
+				// system input
                 Scanner chat        = new Scanner ( System.in ) ;
+				// send out the message
                 PrintWriter out     = new PrintWriter(socket.getOutputStream());
+				// input the next line
                 String serverTalk   = chat.nextLine () ;
-                //out.println("Server send: " + serverTalk);//RESEND IT TO THE CLIENT
-                out.println(serverTalk);//RESEND IT TO THE CLIENT
-                out.flush();//FLUSH THE STREAM
+				//print the server send in data
+                out.println(serverTalk);
+				//flush the stream
+                out.flush();
             }
         } catch ( Exception e ) {
             e.printStackTrace () ;

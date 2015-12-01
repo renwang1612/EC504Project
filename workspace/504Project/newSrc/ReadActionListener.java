7bc1f4a01c53a99fc47877c1181de75108b19244
dyzhangedu <dyzhang@bu.edu>
2015-12-01 13:56:06 +0900

    need modify

--- workspace/504Project/newSrc/ReadActionListener.java
+++ workspace/504Project/newSrc/ReadActionListener.java
@@ -27,7 +27,10 @@ public class ReadActionListener  implements ActionListener{
         try {
             while ((line = in.readLine()) != null) {
                 String parts[] = line.split("\t");// assume tab separated
+                System.out.println("11111");
                 map.put(parts[0], parts[1]); // (parts[0], int)
+                //map.put(line, line); // (parts[0], int)
+                System.out.println(parts[0]+" "+parts[1]);
             }
             in.close();
         } catch ( IOException ex ) {
--- workspace/504Project/newSrc/Receive.java
+++ workspace/504Project/newSrc/Receive.java
@@ -23,7 +23,7 @@ public class Receive extends Subject implements Runnable {
                 //IF THERE IS INPUT THEN MAKE A NEW VARIABLE input AND READ WHAT THEY TYPED
                 System.out.println("Server Said: " + input);//PRINT IT OUT TO THE SCREEN
                 this.notify ( "Server Said: " + input ) ;
-                BufferedWriter writer1 = new BufferedWriter ( new FileWriter ( file ));
+                BufferedWriter writer1 = new BufferedWriter ( new FileWriter ( file, true ));
                 writer1.write (input ) ;
                 writer1.newLine ( ) ;
                 writer1.flush () ;
